b253c508225f9756bc5297492a4e09de2b4d69ff
cuurl <60121726+cuurl@users.noreply.github.com>
2024-12-14 02:42:24 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,33 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    assetsInclude: ['**/*.fbx', '**/*.glb', '**/*.ogg', '**/*.ttf', '**/*.mp3'],
+    assetsInclude: ["**/*.fbx", "**/*.glb", "**/*.ogg", "**/*.ttf", "**/*.mp3"],
     supported: {
         "top-level-await": true, //browsers can handle top-level-await features
     },
     plugins: [
         topLevelAwait({
-          // The export name of top-level await promise for each chunk module
-          promiseExportName: "__tla",
-          // The function to generate import names of top-level await promise in each chunk module
-          promiseImportName: i => `__tla_${i}`
-        })
-      ],
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: "__tla",
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: (i) => `__tla_${i}`,
+        }),
+    ],
     base: "/426-fp/",
+    build: {
+        rollupOptions: {
+            input: {
+                index: "./index.html",
+                death: "./death.html",
+            },
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,http://localhost:4173/426-fp/
+                assetFileNames: `assets/[name].[ext]`,
+            },
+        },
+    },
 });
